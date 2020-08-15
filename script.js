8f4b23c9b8662c340eede51c4ef476152bd0a598
Desmond <desmondaldridge@gmail.com>
2020-08-14 20:25:26 -0700

    Didn't really do much... kind of just tested displaying the current date

---
+++ script.js
@@ -0,0 +1,35 @@
+
+$('#currentDay').append(moment().format('MMM Do YYYY'));
+
+
+
+
+
+
+
+
+
+
+var currentDate = moment().format('MMM Do YYYY');
+var currentDayE1 = $('#currentDay');
+
+currentDayE1.text(currentDate);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
