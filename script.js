af129ddfb9229d69a5cb319863f74ba68a464d77
Desmond <desmondaldridge@gmail.com>
2020-08-19 17:22:05 -0700

    I finally made a moment object the value of a data attribute! It only took 6 days!

--- script.js
+++ script.js
@@ -4,92 +4,76 @@ $('#currentDay').append(moment().format('MMM Do YYYY'));
 // The current hour
 var currentHour = moment().hour();
 
+var timeBlocks = $('.time-block');
+
 // So the time blocks recognize themselves as specific hours of the day... utilizing moment.js as much as possible
-var nineAM = moment('9:00 am', 'hh:mm a').format('h');
-$('#9am').data(nineAM);
+// Funfact: This took me 6 days to figure out
 
+var nineAM = moment('09', 'HH').format('HH');
+$('#9am').data('hour' , nineAM);
+var $9am = $('#9am').data('hour');
 
+var tenAM = moment('10', 'HH').format('HH');
+$('#10am').data('hour' , tenAM);
+var $10am = $('#10am').data('hour');
 
-//Workshopping different methods, syntax, blah blah...
-var tenAM = $('#10am').data('h' , moment().hour(10));
+var elevenAM = moment('11', 'HH').format('HH');
+$('#11am').data('hour' , elevenAM);
+var $11am = $('#11am').data('hour');
 
+var twelvePM = moment('12', 'HH').format('HH');
+$('#12pm').data('hour' , twelvePM);
+var $12pm = $('#12pm').data('hour');
 
-var elevenAM = moment('11:00 am', 'hh:mm a').format('h');
-$('#11am').data('hour', elevenAM);
-var twelvePM = moment('12:00 pm', 'hh:mm a').format('h');
-$('#12pm').data('hour', twelvePM);
-var onePM = moment('1:00 pm', 'hh:mm a').format('h');
-$('#1pm').data('hour', onePM);
-var twoPM = moment('2:00 pm', 'hh:mm a').format('h');
-$('#2pm').data('hour', twoPM);
-var threePM = moment('3:00 pm', 'hh:mm a').format('h');
-$('#3pm').data('hour', threePM);
-var fourPM = moment('4:00 pm', 'hh:mm a').format('h');
-$('#4pm').data('hour', fourPM);
-var fivePM = moment('5:00 pm', 'hh:mm a').format('h');
+var onePM = moment('13', 'HH').format('HH');
+$('#1pm').data('hour' , onePM);
+var $1pm = $('#1pm').data('hour');
+
+var twoPM = moment('14', 'HH').format('HH');
+$('#2pm').data('hour' , twoPM);
+var $2pm = $('#2pm').data('hour');
+
+var threePM = moment('15', 'HH').format('HH');
+$('#3pm').data('hour' , threePM);
+var $3pm = $('#3pm').data('hour');
+
+var fourPM = moment('16', 'HH').format('HH');
+$('#4pm').data('hour' , fourPM);
+var $4pm = $('#4pm').data('hour');
+
+var fivePM = moment('17', 'HH').format('HH');
 $('#5pm').data('hour', fivePM);
+var $5pm = $('#5pm').data('hour');
 
 
+// Color Coordination... literally just learned about the 'each' method yesterday in class... oy, that's so helpful!
+function colorCoordination() {
 
-var $9am = $('#9am');
-var $10am = $('#10am');
-var $11am = $('#11am');
-var $12pm = $('#12pm');
-var $1pm = $('#1pm');
-var $2pm = $('#2pm');
-var $3pm = $('#3pm');
-var $4pm = $('#4pm');
-var $5pm = $('#5pm');
+    timeBlocks.each(function() {
 
-console.log($9am);
-console.log($10am);
-console.log($10am.data);
-console.log(currentHour);
-console.log($10am.data == currentHour);
+      var currentBlock = $(this);
+      var currentBlockMoment = currentBlock.data('hour');
 
+      if (currentBlockMoment < currentHour) {
+        currentBlock.addClass("past").removeClass("present future");
+      }
+      if (currentBlockMoment > currentHour) {
+        currentBlock.addClass("future").removeClass("past present");
+      }
 
+      if (currentBlockMoment === currentHour) {
+        currentBlock.addClass("present").removeClass("past future");
+      }
 
+      console.log(currentBlock.data('hour'));
 
-
-
-
-// Color Coordinator
-function colorCoordinator($h) {
-
-    if ($h < currentHour)
-        $h.addClass('.past');
-    return $h;
-
-
-    // else if (timeID == currentHour)
-    // {$('.time-block').replaceWith('.present');}
-
-    // else if (timeID > currentHour)
-    // {$('.time-block').replaceWith('.future')};
-
+    });
 }
 
-// Color Coordination
+$(document).ready(function(){
 
-colorCoordinator($9am);
-
-
-    // colorCoordinator(tenAM);
-    // colorCoordinator(elevenAM);
-    // colorCoordinator(twelvePM);
-    // colorCoordinator(onePM);
-    // colorCoordinator(twoPM);
-    // colorCoordinator(threePM);
-    // colorCoordinator(fourPM);
-    // colorCoordinator(fivePM);
-
-    // $(".saveBtn").click(function() {
-    //     var toDo = ;
-    //     var hour = ;
-    //     localStorage.setItem()
-    // })
-
-
+colorCoordination();
+})
 
 
 
